2982b5503f48dd9bcf9e2d7e3306e17b61e64b52
ALAxHxC <daniel72584@gmail.com>
2019-06-23 12:31:04 -0500

    organizado proyecto

--- app/app.ts
+++ app/app.ts
@@ -7,7 +7,8 @@ import mongoose from "mongoose";
 import * as bodyParser from "body-parser";
 import { Routes } from "./routes/user";
 import { FileRoutes } from "./routes/files";
-import { ClientRoutes } from "./routes/client"
+import { ClientRoutes } from "./routes/client";
+import { SecurityRoutes } from './routes/security';
 const app: express.Application = express();
 const busboy: any = require('connect-busboy');
 const busboyBodyParser: any = require('busboy-body-parser');
@@ -21,64 +22,14 @@ app.use(busboy());
 app.use(bodyParser.json());
 app.use(busboyBodyParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+const security = new SecurityRoutes()
+security.routes(app)
 const routePrv = new Routes();
 routePrv.routes(app)
 const routesFiles = new FileRoutes();
 routesFiles.routes(app);
 const routesClient = new ClientRoutes();
 routesClient.routes(app);
-
-
-const OAuth2Server = require('oauth2-server'),
-  Request = OAuth2Server.Request,
-  Response = OAuth2Server.Response;
-
-
-app.oauth = new OAuth2Server({
-  model: require('./controllers/security/auth2'),
-  accessTokenLifetime: 60 * 60,
-  allowBearerTokensInQueryString: true,
-
-});
-
-
-function obtainToken(req, res) {
-
-  var request = new Request(req);
-  var response = new Response(res);
-
-  return app.oauth.token(request, response)
-    .then(function (token) {
-
-      res.json(token);
-    }).catch(function (err) {
-
-      res.status(err.code || 500).json(err);
-    });
-}
-
-function authenticateRequest(req, res, next) {
-
-  var request = new Request(req);
-  var response = new Response(res);
-  console.log(req.body)
-  return app.oauth.authenticate(request, response)
-    .then(function (token) {
-
-      next();
-    }).catch(function (err) {
-
-      res.status(err.code || 500).json(err);
-    });
-}
-
-
-app.get('/test', authenticateRequest, function (req, res) {
-
-  res.send('Congratulations, you are in a secret area!');
-});
-app.all('/oauth/token', obtainToken);
 
 
 
--- app/routes/security.ts
+++ app/routes/security.ts
@@ -1,42 +1,67 @@
 import { Request, Response } from "express";
+const OAuth2Server = require('oauth2-server'),
+  Request = OAuth2Server.Request,
+  Response = OAuth2Server.Response;
 
 export class SecurityRoutes {
-
-  public routes(app: any): void {
-    app.route('/')
-      .get((req: Request, res: Response) => {
-        res.status(200).send({
-          message: 'GET request successfulll!!!!'
-        })
-      })
-
-    // Contact 
-    app.route('/client')
-      // GET endpoint 
-      .get((req: Request, res: Response) => {
-        // Get all contacts            
-        contactController.getAllContacts(req, res);
-      })
-      // POST endpoint
-      .post((req: Request, res: Response) => {
-        // Create new contact         
-        contactController.addNewContact(req, res)
-      })
-
-    // Contact detail
-    app.route('/client/:contactId')
-      // get specific contact
-      .get((req: Request, res: Response) => {
-        // Get a single contact detail            
-        contactController.getByIdContact(req, res);
-      })
-      .put((req: Request, res: Response) => {
-        // Update a contact           
-        contactController.updateDocument(req, res)
-      })
-      .delete((req: Request, res: Response) => {
-        // Delete a contact     
-        contactController.deleteDocument(req, res);
-      })
+  app: any;
+  constructor() {
+    this.app = null;
   }
+  public routes(app: any): void {
+    app.oauth = new OAuth2Server({
+      model: require('../controllers/security/auth2'),
+      accessTokenLifetime: 60 * 60,
+      allowBearerTokensInQueryString: true,
+
+    });
+    this.app = app;
+
+    app.get('/test', this.authenticateRequest(app), function (req, res) {
+
+      res.send('Congratulations, you are in a secret area!');
+    });
+    app.all('/oauth/token', this.obtainToken(app));
+
+  }
+
+  public authenticateRequest(app: any) {
+    return (req, res, next) => {
+
+      var request = new Request(req);
+      var response = new Response(res);
+      console.log(req.body)
+      return app.oauth.authenticate(request, response)
+        .then(function (token) {
+
+          next();
+        }).catch(function (err) {
+
+          res.status(err.code || 500).json(err);
+        });
+    }
+  }
+
+  public obtainToken(app: any) {
+    return (req, res) => {
+
+      var request = new Request(req);
+      var response = new Response(res);
+
+      return app.oauth.token(request, response)
+        .then(function (token) {
+
+          res.json(token);
+        }).catch(function (err) {
+
+          res.status(err.code || 500).json(err);
+        });
+    }
+  }
+
+
+
+
+
 }
+
