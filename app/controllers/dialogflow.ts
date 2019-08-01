3b06a19611b7d429684c65a0f3972abb676a4ecf
ALAxHxC <daniel72584@gmail.com>
2019-07-31 22:13:48 -0500

    arreglo de puerto

--- app/controllers/dialogflow.ts
+++ app/controllers/dialogflow.ts
@@ -4,7 +4,7 @@ import { sendMail } from "./providers/sendgrid";
 export const filmentReserve = (req: Request, res: Response) => {
   res.json({
     speech: 'Currently I am not having information about this team',
-    displayText: 'Currently I am not having information about this team',
+    fulfillmentText: 'Currently I am not having information about this team',
     source: 'team info'
   });
 }
